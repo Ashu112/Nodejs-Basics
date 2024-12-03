267e4394d51af3b6363ac4519ae9fd6f2d13ccec
Ashu112 <kashu2611@gmail.com>
2024-12-03 23:29:28 +0530

    activity api

--- UserRoutes/constrollers/users.controller.js
+++ UserRoutes/constrollers/users.controller.js
@@ -1,3 +1,28 @@
-const getUsers = (req, res) => {};
+const axios = require("axios");
 
-module.exports = { getUsers };
+const getUsers = async (req, res) => {
+  try {
+    const URL =
+      "https://gitlab.crio.do/public_content/node-js-sessions/-/raw/master/users.json";
+    const response = await axios.get(URL);
+    //console.log(response.data);
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send({ message: "Please try aagain" });
+  }
+};
+
+const getUserByUuid = async (req, res) => {
+  try {
+    const URL =
+      "https://gitlab.crio.do/public_content/node-js-sessions/-/raw/master/users.json";
+    const response = await axios.get(URL);
+    const users = response.data;
+    console.log(req.params);
+    console.log("users", users);
+  } catch (error) {
+    res.status(500).send({ message: "Something Went wrong, Try Again!!!" });
+  }
+};
+
+module.exports = { getUsers, getUserByUuid };
--- UserRoutes/index.js
+++ UserRoutes/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { getUsers, getUserByUuid } = require("./constrollers/users.controller");
 const app = express();
 const PORT = 8082;
 
@@ -6,4 +7,6 @@ app.listen(PORT, () => {
   console.log("Listening to Port: ", PORT);
 });
 
-//app.get("/users", getUsers);
+app.get("/users", getUsers);
+
+app.get("/users/:uuid", getUserByUuid);
